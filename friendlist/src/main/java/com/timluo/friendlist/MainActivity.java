6c1acb33ece557112850d39f6a3137bc73f3410c
Tim Luo <timoman@gmail.com>
2014-04-12 22:37:17 -0700

    Add/edit contacts, remove guava, and open contact menu on tap as well as long press

--- friendlist/src/main/java/com/timluo/friendlist/MainActivity.java
+++ friendlist/src/main/java/com/timluo/friendlist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.timluo.friendlist;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,12 +12,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.widget.AdapterView.AdapterContextMenuInfo;
 
@@ -25,9 +30,11 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        List<String> list = new ArrayList<String>();
+        list.add("Hello");
+        list.add("world");
         ListAdapter adapter =
-                new ArrayAdapter<String>(this, R.layout.friend_list_item,
-                        Lists.newArrayList("Hello", "world"));
+                new ArrayAdapter<String>(this, R.layout.friend_list_item, list);
         setListAdapter(adapter);
 
         ListView listView = getListView();
@@ -35,12 +42,9 @@ public class MainActivity extends ListActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-
-                Toast.makeText(getApplicationContext(),
-                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+                openContextMenu(view);
             }
         });
-
         registerForContextMenu(listView);
     }
 
@@ -55,7 +59,6 @@ public class MainActivity extends ListActivity {
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-        String contact = (String) getListAdapter().getItem(info.position);
         switch (item.getItemId()) {
             case R.id.delete_contact:
                 deleteContact(info.position);
@@ -76,31 +79,97 @@ public class MainActivity extends ListActivity {
         adapter.remove(adapter.getItem(position));
     }
 
-    void editContact(int position) {
-        Toast.makeText(getApplicationContext(), "Edit Contact", Toast.LENGTH_SHORT).show();
+    void editContact(final int position) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Add Contact");
+        alert.setMessage("Enter a name");
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // This works for strings, but for contacts we'll want to copy over all unedited fields
+                deleteContact(position);
+
+                String value = input.getText().toString();
+                // Do something with value!
+                Log.i(TAG, value);
+                if (value != null && !value.isEmpty()) {
+                    getArrayAdapter().insert(value, position);
+                    refreshListAdapter();
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+        alert.show();
     }
 
     void bumpContact(int position) {
-        @SuppressWarnings("unchecked")
-        ArrayAdapter<String> adapter = ((ArrayAdapter<String>) getListAdapter());
+        ArrayAdapter<String> adapter = getArrayAdapter();
         String contact = adapter.getItem(position);
         // Remove from list, then add to end
         adapter.remove(contact);
         adapter.add(contact);
-        this.runOnUiThread(new Runnable() {
-            public void run() {
-                ((ArrayAdapter) getListAdapter()).notifyDataSetChanged();
-            }
-        });
+        refreshListAdapter();
     }
     /* End Contacts context menu */
 
 
 
+    private void createContact() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Add Contact");
+        alert.setMessage("Enter a name");
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = input.getText().toString();
+                // Do something with value!
+                Log.i(TAG, value);
+                ArrayAdapter<String> adapter = getArrayAdapter();
+                if (value != null && !value.isEmpty()) {
+                    adapter.add(value);
+                    refreshListAdapter();
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+        alert.show();
+    }
+
+    private ArrayAdapter<String> getArrayAdapter() {
+        // "all" and not "unchecked" because adapter will complain as being "redundant"
+        @SuppressWarnings("all")
+        ArrayAdapter<String> adapter = ((ArrayAdapter<String>) getListAdapter());
+        return adapter;
+    }
+
+    private void refreshListAdapter() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                ((ArrayAdapter) getListAdapter()).notifyDataSetChanged();
+            }
+        });
+    }
+
+
     /* Action bar option menu */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -115,7 +184,7 @@ public class MainActivity extends ListActivity {
             case R.id.action_settings:
                 return true;
             case R.id.action_add:
-                // TODO: trigger add contact flow
+                createContact();
         }
         return super.onOptionsItemSelected(item);
     }
